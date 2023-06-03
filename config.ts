be92cfe56332d3a3fec68d55627f91152f0c2174
Kronaldy <135417744+Kronaldy@users.noreply.github.com>
2023-06-03 22:38:57 +0700

    Create config.ts

---
+++ config.ts
@@ -0,0 +1,9 @@
+import * as dotenv from "dotenv";
+
+dotenv.config();
+
+export const botToken: any = process.env.BOT_TOKEN;
+
+export const cyclicDBName = process.env.CYCLIC_DB_NAME;
+
+export const botID = process.env.BOT_ID;
